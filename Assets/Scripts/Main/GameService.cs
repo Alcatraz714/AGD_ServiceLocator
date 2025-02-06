038636ba4eb391accd443c69f0568a8435defd13
Rishi Saxena <rishi.saxena@gamestacy.com>
2025-02-06 13:18:44 +0530

    Updated Service Locator Application

--- Assets/Scripts/Main/GameService.cs
+++ Assets/Scripts/Main/GameService.cs
@@ -1,25 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using ServiceLocator.Player;
+using ServiceLocator.Sound;
+using ServiceLocator.UI;
 using ServiceLocator.Utilities;
 using UnityEngine;
 
 public class GameService : GenericMonoSingleton<GameService>
 {
     public PlayerService playerService {get; private set;}
+    public SoundService soundService {get; private set;}
+
+    [SerializeField] private UIService uIService;
+    public UIService UIService => uIService;
 
     // Services for Service Locator
-    [SerializeField] public PlayerScriptableObject playerScriptableObject;
+    [SerializeField] private PlayerScriptableObject playerScriptableObject;
+    [SerializeField] private SoundScriptableObject soundScriptableObject;
+    [SerializeField] private AudioSource audioEffects;
+    [SerializeField] private AudioSource backgroundMusic;
 
     private void Start() 
     {
         //Init services
-        playerService = new PlayerService(playerScriptableObject);// initial load for SO player service   
+        playerService = new PlayerService(playerScriptableObject);// initial load for SO player service
+        soundService = new SoundService(soundScriptableObject, audioEffects, backgroundMusic);
     }
 
     private void Update() 
     {
         playerService.Update();
-        
     }
 }
